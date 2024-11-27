56ee5bb6fb49436071a5971125a55361f1470e2e
Jonathan Ho <92621215+jxndela@users.noreply.github.com>
2024-11-27 03:46:08 +0000

    Update for 7.11

---
+++ SamplePlugin/Data/BlackjackModes.cs
@@ -0,0 +1,26 @@
+namespace CasinoRoyale.Data;
+
+public enum DealerRules
+{
+    DealerHard17 = 0,
+    DealerSoft17 = 1,
+    DealerHard16 = 2,
+    DealerSoft16 = 3
+}
+
+public static class RuleUtils
+{
+    public static readonly string[] ListOfNames = Enum.GetValues<DealerRules>().Select(n => n.GetName()).ToArray();
+
+    public static string GetName(this DealerRules n)
+    {
+        return n switch
+        {
+            DealerRules.DealerHard17 => "Hard17",
+            DealerRules.DealerHard16 => "Hard16",
+            DealerRules.DealerSoft16 => "Soft16",
+            DealerRules.DealerSoft17 => "Soft17",
+            _ => "Unknown"
+        };
+    }
+}
--- SamplePlugin/Data/Card.cs
+++ SamplePlugin/Data/Card.cs
@@ -19,11 +19,30 @@ public class Card
     }
 
     // Get value of the card for calculations
-    // Returns int 
-    public getValue(int aNumber)
+    // Returns int (true value of card)
+    public int getValue(int aNumber)
     {
         if (aNumber == 1) return 11; // can be 1 in game logic we'll see
         if (aNumber > 10) return 10; // Face cards
         return aNumber;
     }
+
+    // returns card value as a string. Should only be used during initial draws. 
+    public string getValueString (int aNumber)
+    {
+        if (aNumber == 1) return "1/11";
+        if (aNumber > 10) return "10";
+        return number.ToString(aNumber);
+    }
+
+    // Determines if the card is an Ace
+    // Returns a bool, true means it is an Ace, false means it is not an Ace
+    public bool getAce(int aNumber)
+    {
+        if (aNumber == 1) return true;
+        return false;
+    }
+
+    
+
 }
